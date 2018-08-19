350ab2596fa0a8dc946342b2d781afc70702c1cf
Lijinlu1995 <lijinlu1995@foxmail.com>
2018-08-19 17:17:33 +0100

    initialise application, develop the basic model

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function () {
+//    初始化fullpage组件
+    /*设置每一个屏幕的背景颜色*/
+    /*设置屏幕内容的对齐方式 默认是垂直居中的 改成顶部对齐
+    * 设置导航 设置指示器 点容器*/
+    $('.container').fullpage({
+        /*配置参数*/
+        sectionsColor:["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCentered:false,
+        navigation:true,
+        afterLoad:function (link,index) {
+            
+        }
+    })
+})
